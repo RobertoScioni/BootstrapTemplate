ca1e405cc72a5e0fe3abb9d52c520739671588c3
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2020-11-06 01:58:37 +0100

    the docs
    integrated a skeleton of documentation and added a link to point to them

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /**
- * @constant here i'll put any recurrently changing paramater of the template
+ * @constant config
+ * @description here i'll put any recurrently changing paramater of the template
  */
 const config = {
 	title: "hello,Strive!", ///that as of now means only the title but i belive this object will grow soon
 }
-
 /**
  * initialize the page
  *
